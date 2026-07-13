7b8c43ff87785e02c8f0c1a5ea153d78734db41d
Arkar <arkarmin.dev@gmail.com>
2026-07-13 18:28:35 +0700

    remove tracked files that should be ignored

---
+++ server/src/index.ts
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv'
+import express from 'express'
+import cors from 'cors'
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/health',(_req,res)=>{
+    res.json({status:'Server is running'});
+});
+
+
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`);
+});
